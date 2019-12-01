81e2ef745ba7bccaeea59666d9c048d2959ca1d0
Michael Lynch <mtlynch@users.noreply.github.com>
2019-12-01 14:32:45 -0600

    Customizing navigation bar and logo (#6)

--- components/navigation-bar.vue
+++ components/navigation-bar.vue
@@ -1,35 +1,35 @@
 <template>
-  <b-navbar
-    toggleable="md"
-    class="navbar navbar-expand-lg navbar-light bg-light"
-  >
-    <b-navbar-brand v-b-toggle.nav-collapse class="navbar-brand" to="/"
-      >Zestful</b-navbar-brand
-    >
+  <b-navbar toggleable="md" class="navbar navbar-expand-lg custom-navbar">
+    <div class="container">
+      <b-navbar-brand to="/">
+        <img src="/images/logo.png" class="logo" alt="Zestful logo" />
+      </b-navbar-brand>
 
-    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
-    <b-collapse id="nav-collapse" is-nav>
-      <b-navbar-nav class="ml-auto">
-        <b-nav-item to="/">
-          About
-        </b-nav-item>
-        <b-nav-item to="/demo">
-          Demo
-        </b-nav-item>
-        <b-nav-item to="/pricing">
-          Pricing
-        </b-nav-item>
-        <b-nav-item to="/docs">
-          Docs
-        </b-nav-item>
-        <b-nav-item
-          href="https://rapidapi.com/zestfuldata/api/recipe-and-ingredient-analysis"
-        >
-          Get Started
-        </b-nav-item>
-      </b-navbar-nav>
-    </b-collapse>
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item to="/">
+            About
+          </b-nav-item>
+          <b-nav-item to="/demo">
+            Demo
+          </b-nav-item>
+          <b-nav-item to="/pricing">
+            Pricing
+          </b-nav-item>
+          <b-nav-item to="/docs">
+            Docs
+          </b-nav-item>
+          <b-nav-item
+            class="accent-link"
+            href="https://rapidapi.com/zestfuldata/api/recipe-and-ingredient-analysis"
+          >
+            Get Started
+          </b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </div>
   </b-navbar>
 </template>
 
@@ -40,12 +40,21 @@ export default {
 </script>
 
 <style scoped>
-.navbar-brand {
-  margin: 10px 20px 10px 0px;
+.custom-navbar {
+  background: #673ab7;
+  border-bottom: 1px solid #b29bda;
+  margin-bottom: 30px;
 }
 
-.navbar-custom {
-  color: #ffffff;
-  background-color: #cc3333;
+.custom-navbar .nav-link {
+  color: white;
+}
+
+.custom-navbar .accent-link a {
+  color: #ffd740;
+}
+
+.logo {
+  max-height: 47px;
 }
 </style>

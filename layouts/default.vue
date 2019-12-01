642e603d2ee503b5321f55e1dd1499d65a6f5d5e
Michael Lynch <git@mtlynch.io>
2019-12-01 16:32:22 -0500

    Adding more content, tweaking styles

--- layouts/default.vue
+++ layouts/default.vue
@@ -12,5 +12,35 @@ export default {
   components: {
     NavigationBar,
   },
+  link: [
+    {
+      rel: 'stylesheet',
+      href:
+        'https://fonts.googleapis.com/css?family=Calistoga|Rubik&display=swap',
+    },
+  ],
 };
 </script>
+
+<style>
+body {
+  font-family: 'Rubik', sans-serif;
+}
+
+h1,
+h2,
+h3,
+h4,
+h5 {
+  font-family: 'Calistoga', serif;
+}
+
+h1 {
+  margin-bottom: 20px;
+}
+
+h2 {
+  margin-top: 70px;
+  margin-bottom: 30px;
+}
+</style>

58e10800435fb6717f9ad4ac39e502fe8a9d54c4
Michael Lynch <mtlynch@users.noreply.github.com>
2019-12-01 13:42:02 -0500

    Initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="app">
+    <div class="nav horizontal-align">
+      <nuxt-link class="nav-item" to="/">
+        Home
+      </nuxt-link>
+      <nuxt-link class="nav-item" to="/counter">
+        Counter
+      </nuxt-link>
+    </div>
+    <nuxt class="container" />
+  </div>
+</template>
+
+<style>
+body,
+html {
+  padding: 0;
+  margin: 20px;
+  min-height: 100vh;
+}
+.nav-item {
+  text-decoration: none;
+  font-size: 1.2em;
+}
+</style>

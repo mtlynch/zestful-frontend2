a322572af32f24ff2f8d8a5bfc20b5b73ac15a89
Michael Lynch <mtlynch@users.noreply.github.com>
2019-12-01 14:09:42 -0600

    Setting favicon (#5)

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,6 +9,13 @@ export default {
       {name: 'viewport', content: 'width=device-width, initial-scale=1.0'},
     ],
   },
+  link: [
+    {
+      rel: 'icon',
+      type: 'image/x-icon',
+      href: 'favicon.ico',
+    },
+  ],
   modules: ['@nuxtjs/sitemap', 'bootstrap-vue/nuxt'],
   sitemap: {
     hostname: 'https://zestfuldata.com/',

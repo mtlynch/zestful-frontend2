63458b0f81d35c87795dd050d86405d630a04feb
Michael Lynch <git@mtlynch.io>
2019-12-02 11:32:26 -0500

    Adding favicons

--- nuxt.config.js
+++ nuxt.config.js
@@ -33,6 +33,32 @@ export default {
         type: 'image/x-icon',
         href: 'favicon.ico',
       },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '16x16',
+        href: '/images/favicons/favicon-16x16.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '32x32',
+        href: '/images/favicons/favicon-32x32.png',
+      },
+      {
+        rel: 'apple-touch-icon',
+        sizes: '180x180',
+        href: '/images/favicons/apple-touch-icon.png',
+      },
+      {
+        rel: 'mask-icon',
+        color: '#5bbad5',
+        href: '/images/favicons/safari-pinned-tab.svg',
+      },
+      {
+        rel: 'manifest',
+        href: 'site.webmanifest',
+      },
     ],
   },
   buildModules: [

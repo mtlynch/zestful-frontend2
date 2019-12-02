b2082e6567648d7cb53e7f20eb5bf4db4d00c03c
Michael Lynch <mtlynch@users.noreply.github.com>
2019-12-02 09:57:48 -0600

    Adding opengraph metadata (#43)

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,6 +10,22 @@ export default {
     meta: [
       {charset: 'utf-8'},
       {name: 'viewport', content: 'width=device-width, initial-scale=1.0'},
+      {
+        name: 'description',
+        content:
+          'A machine-learning-powered ingredient parser API. Parse your recipe ingredients into clean, structured JSON. Built to power the best food apps online.',
+      },
+      {
+        name: 'og:description',
+        content:
+          'A machine-learning-powered ingredient parser API. Parse your recipe ingredients into clean, structured JSON. Built to power the best food apps online.',
+      },
+      {
+        name: 'og:image',
+        content: 'https://zestfuldata.com/images/ingredient-translation.jpg',
+      },
+      {name: 'og:image:width', content: '635'},
+      {name: 'og:image:height', content: '558'},
     ],
     link: [
       {
